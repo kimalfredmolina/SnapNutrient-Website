0b67f8628f50e3160f9e88838ec8ffd0ff562527
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-10-11 15:19:59 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white text-3xl font-bold">
+      Hello, Tailwind + React! 💙
+    </div>
+  )
+}
+
+export default App
