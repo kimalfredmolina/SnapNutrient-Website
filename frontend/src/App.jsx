dc91dbae12c7d5526bfa4dd7027d2bef3a2c65db
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-10-17 20:41:33 +0800

    Updated the design and align color way in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className='relative min-h-screen'>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#06b6d4_100%)]"></div>
       <div>
           
-
        <div className="relative z-10">
           <Navbar />
           <div id='home_page'>
@@ -14,7 +14,6 @@ const App = () => {
           </div>
 
        </div>
-
 
       </div>
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { motion } from "framer-motion";
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'; 
-import logo from '../assets/icon.png';
+import logo from '../assets/transparent_banner.png';
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full text-lime-400 z-50 font-poppins bg-transparent">
+    <div className="fixed top-0 left-0 w-full text-emerald-500 z-50 font-poppins bg-transparent">
       <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-10">
         <div className="flex justify-between items-center h-20">
           <div className="flex-shrink-0">
@@ -20,10 +20,10 @@ const Navbar = () => {
             </a>
           </div>
           <div className="hidden md:flex flex-1 justify-center items-center space-x-8 lg:space-x-12 text-lg lg:text-2xl">
-            <a href="#home" className="hover:text-red-600 font-bold duration-300">Home</a>
-            <a href="#about" className="hover:text-red-600 font-bold duration-300">Features</a>
-            <a href="#skills" className="hover:text-red-600 font-bold duration-300">About</a>
-            <a href="#exp" className="hover:text-red-600 font-bold duration-300">Contact</a>
+            <a href="#home" className="hover:text-cyan-300 font-bold duration-300">Home</a>
+            <a href="#about" className="hover:text-cyan-300 font-bold duration-300">Features</a>
+            <a href="#skills" className="hover:text-cyan-300 font-bold duration-300">About</a>
+            <a href="#exp" className="hover:text-cyan-300 font-bold duration-300">Contact</a>
           </div>
           <div className="md:hidden flex items-center">
             <button onClick={toggleNav} className="focus:outline-none">
@@ -39,12 +39,12 @@ const Navbar = () => {
           initial={{ opacity: 0, height: 0 }}
           animate={{ opacity: 1, height: 'auto' }}
           transition={{ duration: 0.3 }}
-          className="md:hidden bg-gray-800 text-lime-400 text-lg absolute w-full left-0 top-20 z-40 shadow-lg">
+          className="md:hidden bg-gray-800 text-green-500 text-lg absolute w-full left-0 top-20 z-40 shadow-lg">
           <div className="flex flex-col items-center py-4 space-y-4">
-            <a href="#home" onClick={toggleNav} className="hover:text-red-600 font-bold duration-300">Home</a>
-            <a href="#about" onClick={toggleNav} className="hover:text-red-600 font-bold duration-300">Features</a>
-            <a href="#skills" onClick={toggleNav} className="hover:text-red-600 font-bold duration-300">About</a>
-            <a href="#exp" onClick={toggleNav} className="hover:text-red-600 font-bold duration-300">Contact</a>
+            <a href="#home" onClick={toggleNav} className="hover:text-cyan-300 font-bold duration-300">Home</a>
+            <a href="#about" onClick={toggleNav} className="hover:text-cyan-300 font-bold duration-300">Features</a>
+            <a href="#skills" onClick={toggleNav} className="hover:text-cyan-300 font-bold duration-300">About</a>
+            <a href="#exp" onClick={toggleNav} className="hover:text-cyan-300 font-bold duration-300">Contact</a>
           </div>
         </motion.div>
       )}
